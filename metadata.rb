238b6e4880300a080f892b270bf1371fa6fdfdaa
Brad Dausses <brad.dausses@gmail.com>
2019-02-03 23:52:31 -0600

    added ssh-hardening and line edit for auditd.conf
    Signed-off-by: Brad Dausses <brad.dausses@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -18,3 +18,7 @@ chef_version '>= 13.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/verifier_example'
+
+depends 'line'
+depends 'os-hardening'
+depends 'ssh-hardening'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2019, Chef Software, All Rights Reserved.
+
+include_recipe 'os-hardening::default'
+
+replace_or_add 'auditd.conf update' do
+  path '/etc/audit/auditd.conf'
+  pattern 'max_log_file_action.*'
+  line 'max_log_file_action = keep_logs'
+end
